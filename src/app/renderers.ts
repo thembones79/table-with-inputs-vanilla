2e25c18ff4aeec140bdfcab775d655663bc8e6ef
thembones79 <michal.szumnarski@gmail.com>
2025-11-19 07:00:46 +0100

    work in progress

--- src/app/renderers.ts
+++ src/app/renderers.ts
@@ -1,12 +1,7 @@
 // Renderers perform DOM manipulations
 // Name starts with render
 
-import {
-  Footer,
-} from "./components";
-
 export const reRenderFooter = () => {
   const footer = document.querySelector(".footer");
   if (!footer) return;
-  footer.outerHTML = Footer();
 };
